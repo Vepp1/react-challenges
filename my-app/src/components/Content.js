d11575acdc702a904554092cea99c70eaca07377
Vepp1 <vinipontarolla@gmail.com>
2022-12-07 16:21:48 +0000

    Component challenge

--- my-app/src/components/Content.js
+++ my-app/src/components/Content.js
@@ -2,10 +2,24 @@ import React, { Component } from 'react'
 import css from './css/Content.module.css'
 import {savedPosts} from './../posts.json'
 import PostItem from './PostItem'
+import Loader from './Loader'
 
 export class Content extends Component {
     constructor(props) {
       super(props)
+
+      this.state = {
+        isLoaded: false
+      }
+    }
+
+    componentDidMount() {
+      setTimeout(() => {
+        console.log('Render methor called')
+        this.setState({
+            isLoaded: true
+        })
+    }, 2000)
     }
 
   render() {
@@ -15,12 +29,16 @@ export class Content extends Component {
           <h1>My Photos</h1>
         </div>
 
-        <div className={css.SearchResults}>
+        {this.state.isLoaded ? 
+        (<div className={css.SearchResults}>
             {savedPosts.map(post => {
               return <PostItem key={post.title} post={post} />
               
             })}
-        </div>
+        </div>) : 
+        (<Loader />)}
+        
+
       </div>
     )
   }
